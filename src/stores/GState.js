68c54745a84247caafdbb33c0b745ec9e041534b
doriaarnaud <doriaarnaud55@gmail.com>
2023-08-05 07:12:45 +0200

    image source update

--- src/stores/GState.js
+++ src/stores/GState.js
@@ -7,15 +7,15 @@ export const useProductTab=defineStore('product_tab',//this is refered as 'produ
   ({
     product_tab:
     [
-      {id:0,idt:'f2eh4y7fbhx684g',price:1,image:'src/assets/Products/Product1.png',title:'Gm1',description:'This is full product description'},
-      {id:1,idt:'f2eh4y7fbhx684g',price:2,image:'src/assets/Products/Product2.png',title:'Gm12',description:'This is full product description'},
-      {id:2,idt:'f2eh4y7fbhx684g',price:3,image:'src/assets/Products/Product3.png',title:'Gm123',description:'This is full product description'},
-      {id:3,idt:'f2eh4y7fbhx684g',price:4,image:'src/assets/Products/Product6.png',title:'Gm1432',description:'This is full product description'},
-      {id:4,idt:'f2eh4y7fbhx684g',price:5,image:'src/assets/Products/Product5.png',title:'Gm143653',description:'This is full product description'},
-      {id:5,idt:'f2eh4y7fbhx684g',price:6,image:'src/assets/Products/Product4.png',title:'Gm165764',description:'This is full product description'},
-      {id:6,idt:'f2eh4y7fbhx684g',price:7,image:'src/assets/Products/Product7.png',title:'Gm',description:'This is full product description'},
-      {id:7,idt:'f2eh4y7fbhx684g',price:8,image:'src/assets/Products/Product8.png',title:'Gm',description:'This is full product description'},
-      {id:8,idt:'f2eh4y7fbhx684g',price:9,image:'src/assets/Products/Product9.png',title:'Gm',description:'This is full product description'}
+      {id:0,idt:'f2eh4y7fbhx684g',price:1,image:'assets/Products/Product1.png',title:'Gm1',description:'This is full product description'},
+      {id:1,idt:'f2eh4y7fbhx684g',price:2,image:'assets/Products/Product2.png',title:'Gm12',description:'This is full product description'},
+      {id:2,idt:'f2eh4y7fbhx684g',price:3,image:'assets/Products/Product3.png',title:'Gm123',description:'This is full product description'},
+      {id:3,idt:'f2eh4y7fbhx684g',price:4,image:'assets/Products/Product6.png',title:'Gm1432',description:'This is full product description'},
+      {id:4,idt:'f2eh4y7fbhx684g',price:5,image:'assets/Products/Product5.png',title:'Gm143653',description:'This is full product description'},
+      {id:5,idt:'f2eh4y7fbhx684g',price:6,image:'assets/Products/Product4.png',title:'Gm165764',description:'This is full product description'},
+      {id:6,idt:'f2eh4y7fbhx684g',price:7,image:'assets/Products/Product7.png',title:'Gm',description:'This is full product description'},
+      {id:7,idt:'f2eh4y7fbhx684g',price:8,image:'assets/Products/Product8.png',title:'Gm',description:'This is full product description'},
+      {id:8,idt:'f2eh4y7fbhx684g',price:9,image:'assets/Products/Product9.png',title:'Gm',description:'This is full product description'}
     ],
     directitemqtt:0,//used to initialize the 'itemview' quantity 
     searchtext:''//v-model for the input search in header page
